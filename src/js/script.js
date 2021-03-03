d1b4a2462f1c348ef2a74e4e144820d2bd3defb2
Barbara PakulaE <barbara.pakula@hotmail.com>
2021-03-03 16:42:16 +0100

    add general statistic section

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,10 @@ const handleClick = () => {
 hamburger.addEventListener('click', handleClick);
 
 
+let today = new Date().toISOString().substr(0, 10);
+document.querySelector('.date_picker').value = today;
+
+document.querySelector('.date_picker').valueAsDate = new Date();
+
+
 
