3963b6c863dda6831c9ec98aed25f20955072f80
Barbara PakulaE <barbara.pakula@hotmail.com>
2021-03-10 15:20:59 +0100

    add  js responsive chart

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+/* sidbar &hamburger button    */
 const hamburger_display = document.querySelector('.hamburger');
 const navVisable = document.querySelector('#wrapper');
 
@@ -9,12 +10,12 @@ const handleClick = (event) => {
 
 hamburger_display.addEventListener('click', handleClick);
 
-// input date
+/* input date*/
 let today = new Date().toISOString().substr(0, 10);
 document.querySelector('.date_picker').value = today;
 document.querySelector('.date_picker').valueAsDate = new Date();
 
-// /* Active pages */
+/* Active pages */
 
 const pages = document.querySelector('#page-content-wrapper').children;
 const navLinks = document.querySelectorAll('#sidebar-wrapper a');
@@ -60,7 +61,7 @@ initPages();
 
 
 
-// /* modal */
+/* modal */
 // prostej funkcji, która zamknie modal poprzez usunięcie klasy show z overlaya.
 function closeModal() {
   document.getElementById('overlay').classList.remove('show');
@@ -94,7 +95,6 @@ function openModal(modal) {
   document.querySelector('#overlay').classList.add('show');
   document.querySelector(modal).classList.add('show');
 }
-console.log(document.querySelectorAll('.icon-profile'));
 
 const icon_quit = document.querySelectorAll('.icon-quit ');
 for (let quit of icon_quit) {
@@ -110,3 +110,39 @@ for (let quit of icon_profile) {
     openModal('#icon-profile');
   });
 }
+
+/*canvas*/
+var ctx = document.getElementById('myChart').getContext('2d');
+var Chart = new Chart(ctx, {
+  // 1
+  type: 'bar',
+  data: {
+    // 2
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    // 3
+    datasets: [{
+      // 4
+      label: 'Signups',
+      // 5
+      backgroundColor: '#8DBEC8',
+      borderColor: '#8DBEC8',
+      // 6
+      data: [250, 200, 250, 300, 400, 350, 230, 190, 180],
+    },
+    {
+      label: 'FTD',
+      backgroundColor: '#F29E4E',
+      borderColor: '#F29E4E',
+      data: [350, 180, 200, 390, 410, 110, 200, 440, 200],
+    },
+    {
+      label: 'Earned',
+      backgroundColor: '#71B374',
+      borderColor: '#71B374',
+      data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+      // 7
+      // hidden: true,
+    }]
+  },
+});
+
